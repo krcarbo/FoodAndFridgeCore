bca3a917beea152397b023959aa07c0fdbc7cc43
GUENET KEVIN <kevin.guenet@etu.unistra.fr>
2018-10-11 22:46:32 +0200

    Ajoutez des fichiers projet.

---
+++ FoodAndFridgeCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodAndFridgeCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
