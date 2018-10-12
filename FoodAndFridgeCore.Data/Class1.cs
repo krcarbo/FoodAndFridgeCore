e1a14e358aba62efde8363edf4e7f981a013907a
GUENET KEVIN <kevin.guenet@etu.unistra.fr>
2018-10-12 12:14:06 +0200

    Ajout des librairies

---
+++ FoodAndFridgeCore.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FoodAndFridgeCore.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ FoodAndFridgeCore.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FoodAndFridgeCore.Logic
+{
+    public class Class1
+    {
+    }
+}
